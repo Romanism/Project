9cfee00869c98ab3b3bc4cac96af847259f2c5ee
Romanism <jsi920629@gmail.com>
2018-05-18 23:21:50 +0900

    update

---
+++ 02_Coupon Purchase Prediction/function.py
@@ -0,0 +1,15 @@
+# 0. basic
+import numpy as np
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+
+# 1. EDA
+import seaborn as sns
+sns.set()
+sns.set_style("whitegrid")
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.pyplot import *
+
+import datetime as dt
